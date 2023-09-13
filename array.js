bc29ee0e6cea5ea5b61f41b6a6a04e09fae97071
liannealkhaled <liannealq@hotmail.com>
2023-09-13 19:12:18 +0300

    array task 1

---
+++ array.js
@@ -0,0 +1,37 @@
+// let myarray = [`apple`, `banana`, `cherry`];
+// console.log(myarray.slice(0, 1));
+// console.log(myarray.slice(2));
+// myarray.unshift(`mango`);
+// console.log(myarray.length);
+
+// myarray.pop();
+
+// console.log(myarray);
+
+// let myarray = []
+// myarray.push(`apple`, `banana`, `cherry`)
+// console.log(myarray[0])
+// console.log(myarray[myarray.length-1])
+// myarray.unshift(`mango`)
+// myarray.pop()
+
+let myarray2 = [5, 10, 15, 20, 25];
+myarray2.splice(2, 1);
+console.log(myarray2);
+console.log(myarray2.length);
+
+const tempretures = [72, 68, 74, 80, 76];
+console.log(Math.max(...tempretures));
+
+temp.sort();
+console.log(temp);
+temp.slice[4];
+
+function fruits(index) {
+  if (index == `banana`) {
+    return true;
+  } else {
+    false;
+  }
+}
+console.log(fruits(`banana`));
